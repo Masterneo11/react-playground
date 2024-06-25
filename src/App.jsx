8cbbc9d680935d18e77406303e6ffa8c75dfbc24
Mathew Zamacona <mateoszamacona@icloud.com>
2024-06-25 09:26:03 -0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>hello</h1>
+      <nav>
+        <a href="#"> link 1</a>
+        <a href="#"> link 2</a>
+        <a href="#"> link 3</a>
+
+
+      </nav>
+    </>
+  )
+}
+
+export default App
